13f62ea9067066e925c66242eea5bed3ceddc3d5
Lucas Hild <contact@lucas-hild.de>
2019-03-10 18:29:46 +0100

    Add methods to base channel

--- mediathek/channels/base.py
+++ mediathek/channels/base.py
@@ -1,3 +1,9 @@
 class BaseChannel:
     def all_videos(self):
         raise NotImplementedError
+
+    def video(self, id):
+        raise NotImplementedError
+
+    def all_brands(self):
+        raise NotImplementedError
