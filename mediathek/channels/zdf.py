056d9f13e3ad80a6a3a36df6719f5941555c0d88
Lucas Hild <contact@lucas-hild.de>
2019-04-24 21:02:36 +0200

    Add API key fetching for zdf

--- mediathek/channels/zdf.py
+++ mediathek/channels/zdf.py
@@ -1,3 +1,4 @@
+import re
 import requests
 
 from .base import BaseChannel
@@ -7,9 +8,18 @@ from ..models import Video, VideoResolution, Brand
 class Zdf(BaseChannel):
     BASE_API_URL = "https://zdf-cdn.live.cellular.de/mediathekV2"
 
+    def __init__(self):
+        self.api_key = self.get_api_key()
+
     def video(self, id):
         r = requests.get(f"{self.BASE_API_URL}/document/{id}")
         return ZdfVideo(r.json()["document"], channel=self)
+
+    def get_api_key(self):
+        """The API key is present in a <script> tag of the mediathek index"""
+        r = requests.get("https://www.zdf.de/")
+        z = re.search(r"apiToken: '(\S+)'", r.text)
+        return z.group(1)
 
     @staticmethod
     def _parse_date(date):
